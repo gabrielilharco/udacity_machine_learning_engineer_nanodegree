22af9458cc3599112bad7b3e196564f30e836312
gabrielilharco <gabriel.ilharco@gmail.com>
2016-09-15 15:40:15 -0300

    Revert "changes after review"
    This reverts commit 38d4bc8aed20c622e43309301f1ea48efd2cdd4c.

--- 4 - Smartcab/smartcab/agent.py
+++ 4 - Smartcab/smartcab/agent.py
@@ -10,7 +10,7 @@ class LearningAgent(Agent):
         super(LearningAgent, self).__init__(env)  # sets self.env = env, state = None, next_waypoint = None, and a default color
         self.color = 'red'  # override color
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
-        self.actions = self.env.valid_actions
+       
         self.Q_learner = {}
         self.reward = 0
 
@@ -27,22 +27,26 @@ class LearningAgent(Agent):
         # Update state
         self.state = (inputs['light'], inputs['oncoming'], inputs['left'], inputs['right'], self.next_waypoint)
 
-        # Select action according to policy
-        # get the maximum Q-value for the state
-        max_qval = max([self.Q_learner.get((self.state, action),0) for action in self.actions])
-        # select the actions that yield the maximum Q-value
-        best_actions = [action for action in self.actions if self.Q_learner.get((self.state, action),0) == max_qval]
-        # randomly pick one of the best actions
-        action = random.choice(best_actions)
+        # Select action according to your policy
+        possible_actions = {}
+        for action in self.env.valid_actions:
+            possible_actions[action] = self.Q_learner.get((self.state, action), 0)
+        max_utility = max(possible_actions.values())
+
+        best_actions = []
+        for action in self.env.valid_actions:
+            if possible_actions[action] == max_utility:
+                best_actions.append(action)
+        best_action = random.choice(best_actions)
 
         # Execute action and get reward
-        reward = self.env.act(self, action)
+        reward = self.env.act(self, best_action)
         self.reward += reward
 
         # Learn policy based on state, action, reward
         alpha = 1.0
-        self.Q_learner [(self.state, action)] = \
-            (1-alpha)*self.Q_learner.get((self.state, action),0) + \
+        self.Q_learner [(self.state, best_action)] = \
+            (1-alpha)*self.Q_learner.get((self.state, best_action),0) + \
             alpha*reward
 
         #print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
